20dfdc2e539e07ca1344ce38e904771702f39bae
sumeel <elyssasumendap@gmail.com>
2025-02-03 21:28:27 -0800

    final changes (?)

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 
 function showMessage(response) {
   if (response === "No") {
-    // Remove name message and no button
-    document.getElementById("name").remove();
-    document.getElementById("noButton").remove();
+    //hide no button
+    document.getElementById("noButton").style.display = "none";
 
     // Update text content, show message, and change image source to "dance.gif"
-    const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "erm...please?";
-    yesMessage.style.display = "block";
-    yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "img/dead.png";
-    document.body.style.backgroundColor = "black";
+    const noName = document.getElementById("name");
+    const noMessage = document.getElementById("question");
+    noName.textContent = "but pookie...";
+    noMessage.textContent = "erm...please?";
+    noMessage.style.display = "block";
+    noMessage.style.fontStyle = "normal";
+    
+    const image = document.getElementsByClassName("image")[0];
+    image.src = "img/dead.png";
+    image.style.width = "450px";
+    image.style.height = "300px";
 
-    // Remove yes button
-    //document.getElementById("yesButton").remove();
+    document.body.style.backgroundColor = "black";
+    document.getElementById("yesButton").style.display = "block";
   }
   if (response === "Yes") {
     // Remove name message and no button
@@ -27,7 +31,11 @@ function showMessage(response) {
     yesMessage.textContent = "you're gay. see you (virtually) on the 14th pookie <3";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "img/dance.gif";
+    const image = document.getElementsByClassName("image")[0];
+    image.src = "img/dance.gif";
+    image.style.width = "350px";
+    image.style.height = "525px";
+    document.body.style.backgroundColor = "#ff7f7f";
 
   // Remove yes button
     document.getElementById("yesButton").remove();
